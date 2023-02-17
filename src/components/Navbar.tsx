a6c4e1da0b98661629e697bf3c34d9ee31c527b9
Hwang Seok O <ohs6006@gmail.com>
2023-02-17 12:50:19 +0900

    폰트 설정 & 브랜드 컬러 설정

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="flex justify-between border-b border-gray-300 p-2">
+      Navbar 네비게이션 바
+    </header>
+  );
 };
