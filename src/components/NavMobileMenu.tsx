a6ac70f6d3f48ee6895af36a987973eeda2d33fa
Hwang Seok O <ohs6006@gmail.com>
2023-02-21 02:17:38 +0900

    모바일 nav 완료

--- src/components/NavMobileMenu.tsx
+++ src/components/NavMobileMenu.tsx
@@ -1,33 +1,80 @@
 import React from "react";
 import { IoMdClose } from "react-icons/io";
+import { Link, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 interface NavMobileType {
-  handleClick: React.MouseEventHandler<HTMLButtonElement>;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
   isOpen: boolean;
 }
 
-export const NavMobileMenu = ({ handleClick, isOpen }: NavMobileType) => {
+const commonPathClass = `flex w-full basis-full items-center gap-10 text-red-500 
+            before:h-1 before:flex-1 before:bg-red-500 before:content-[''] before:transition-colors
+            after:h-1 after:flex-1 after:bg-red-500 after:content-['']
+            `;
+
+export const NavMobileMenu = ({ setIsOpen, isOpen }: NavMobileType) => {
+  const location = useLocation();
+  const navigation = useNavigate();
+
+  const handleNav = (url: string): void => {
+    setIsOpen(!isOpen);
+    navigation(url);
+  };
+
   return (
     <ul
       className={
         isOpen
-          ? "fixed top-0 z-10 flex w-screen flex-col items-center justify-end gap-5 bg-primaryBg py-20 text-2xl duration-300"
-          : "fixed top-[-500px] z-10 flex w-screen flex-col items-center justify-end gap-5 bg-primaryBg py-20 text-2xl duration-300"
+          ? "group fixed top-0 z-10 flex w-screen flex-col items-center justify-end gap-5 bg-primaryBg py-20 text-2xl duration-300"
+          : "fixed top-[-800px] z-10 flex w-screen flex-col items-center justify-end gap-5 bg-primaryBg py-20 text-2xl duration-300"
       }
     >
+      <li className="bg-"></li>
       <button
-        onClick={handleClick}
-        className="absolute right-8 top-8 rounded-full  p-2 text-3xl hover:bg-primaryHover"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+        className="absolute right-8 top-8 rounded-full p-2 text-3xl hover:bg-primaryHover "
       >
         <IoMdClose />
       </button>
-      <li>날씨</li>
-      <li>유머</li>
-      <li>OTT</li>
-      <li>SNS</li>
-      <li>쇼핑</li>
-      <li>스포츠</li>
-      <li>음악</li>
+      <img src="/logo.png" alt="logo" className="aspect-square w-7" />
+      <h1 className="">What's Trends?</h1>
+      <li
+        className={location.pathname === "/" ? commonPathClass : ""}
+        onClick={() => handleNav("/")}
+      >
+        홈
+      </li>
+      <li
+        className={
+          location.pathname === "/weather"
+            ? commonPathClass
+            : "commontNoPathClass"
+        }
+        onClick={() => handleNav("/weather")}
+      >
+        날짜
+      </li>
+      <li className={location.pathname === "/humor" ? commonPathClass : ""}>
+        유머
+      </li>
+      <li className={location.pathname === "/ott" ? commonPathClass : ""}>
+        OTT
+      </li>
+      <li className={location.pathname === "/sns" ? commonPathClass : ""}>
+        SNS
+      </li>
+      <li className={location.pathname === "/shopping" ? commonPathClass : ""}>
+        쇼핑
+      </li>
+      <li className={location.pathname === "/sports" ? commonPathClass : ""}>
+        스포츠
+      </li>
+      <li className={location.pathname === "/music" ? commonPathClass : ""}>
+        음악
+      </li>
     </ul>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ export const Navbar = () => {
 
   return (
     <>
-      <header className="hidden h-screen justify-between bg-secondaryBg p-10 text-white sm:flex">
+      <header className="hidden h-screen justify-between p-10 text-white sm:flex">
         <h1 className="font-sans text-4xl">What's Trends?</h1>
       </header>
       <header className="flex w-screen items-center py-4 px-3 text-2xl sm:hidden">
@@ -25,7 +25,7 @@ export const Navbar = () => {
           <img src="/logo.png" alt="logo" className="aspect-square w-7" />
         </Link>
       </header>
-      <NavMobileMenu handleClick={handleClick} isOpen={isOpen} />
+      <NavMobileMenu setIsOpen={setIsOpen} isOpen={isOpen} />
     </>
   );
 };
