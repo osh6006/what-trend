6590ff84d3eca620014c140a7c0024d6cbf2e313
Hwang Seok O <ohs6006@gmail.com>
2023-03-03 23:26:45 +0900

    weather layout

---
+++ src/components/weather/Search.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Search = () => {
+  return (
+    <>
+      <input
+        type="text"
+        name="search"
+        id="search"
+        placeholder="Search new place"
+      />
+    </>
+  );
+};
--- src/pages/Weather.tsx
+++ src/pages/Weather.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Search } from "../components/weather/Search";
 
 export default function Weather() {
   return (
     <main className="ml-0 flex h-screen w-full flex-1 flex-col py-4 sm:ml-72 lg:ml-96 lg:flex-row lg:pr-10">
-      Weather
+      <section className="w-full  p-5 shadow-lg lg:w-6/12 lg:min-w-[500px]">
+        <Search />
+      </section>
+      <section className="w-full rounded-r-2xl border-r-2 bg-slate-900 p-5 shadow-lg lg:w-5/12 lg:min-w-[500px]"></section>
     </main>
   );
 }
