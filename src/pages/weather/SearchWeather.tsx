c00f4a0e649f60a55e21ad05bac074d95b5159dd
Hwang Seok O <ohs6006@gmail.com>
2023-03-27 21:37:20 +0900

    test

--- src/pages/weather/SearchWeather.tsx
+++ src/pages/weather/SearchWeather.tsx
@@ -29,6 +29,7 @@ export const SearchWeather = () => {
             </section>
             <section className="w-full overflow-hidden border-r-2 bg-secondaryBg p-5 shadow-lg lg:w-[45%] lg:min-w-[600px] 2xl:rounded-r-2xl">
               <Today weatherQuery={weatherQuery} />
+              <div></div>
             </section>
           </>
         )}
