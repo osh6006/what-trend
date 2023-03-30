8d69a1fde23824e76792008b2ac214db2faa092b
Hwang Seok O <ohs6006@gmail.com>
2023-03-30 22:04:46 +0900

    shopping search style

--- src/components/shopping/ShoppingSearch.tsx
+++ src/components/shopping/ShoppingSearch.tsx
@@ -1,5 +1,47 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
+import { Loading } from "../common/Loading";
 
 export const ShoppingSearch = () => {
-  return <div>Search</div>;
+  const navigate = useNavigate();
+  const [search, setSearch] = useState<string>("");
+  const handlechange = (e: React.FormEvent<HTMLInputElement>): void => {
+    setSearch(e.currentTarget.value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    if (search !== "") {
+      // search start!
+    }
+  };
+
+  return (
+    <form
+      onSubmit={onSubmit}
+      className="relative mt-2 flex w-full items-center justify-center pt-2 text-xl text-gray-400"
+    >
+      <div className="relative w-1/2">
+        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+          <HiOutlineSearch />
+        </div>
+        <input
+          type="search"
+          id="default-search"
+          className="block w-full rounded-lg border-2 border-gray-300 p-4 pl-10 text-base text-gray-900 focus:border-gray-500 focus:ring-gray-500"
+          placeholder="Search new Something"
+          onChange={handlechange}
+          required
+        />
+        <button
+          type="submit"
+          className="absolute right-3.5 bottom-3.5  rounded-lg bg-black px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-secondaryBg focus:outline-none focus:ring-4 focus:ring-gray-600"
+        >
+          Search
+        </button>
+      </div>
+    </form>
+  );
 };
--- src/pages/shopping/Shopping.tsx
+++ src/pages/shopping/Shopping.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { ShoppingSearch } from "../../components/shopping/ShoppingSearch";
 
 export default function Shopping() {
   return (
     <main className='className="mt-0 xl:flex-row" ml-0 flex h-full w-full flex-col overflow-auto py-2 sm:ml-[250px] sm:pt-8 lg:ml-[344px] lg:h-screen'>
-      <h1 className="text-4xl font-bold">Shopping Trends</h1>
+      <h1 className="text-center text-4xl font-bold">Shopping Trends</h1>
       {/* search bar */}
+      <ShoppingSearch />
     </main>
   );
 }
