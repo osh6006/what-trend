b3571188ae573b5137a3afa40fc1cd7a4f636f3b
Hwang Seok O <ohs6006@gmail.com>
2023-03-30 22:52:51 +0900

    scroll bar style modify

---
+++ src/components/shopping/ShoppingGrid.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const div: string = "space-y-3";
+
+export const ShoppingGrid = () => {
+  return (
+    <section className="mt-20 grid w-full grid-cols-3 gap-4 px-5">
+      <div className={div}>
+        <h1 className="text-center text-lg font-bold uppercase text-green-500">
+          Naver
+        </h1>
+        <ul className="space-y-4 border-4 p-5">
+          <li className="flex items-center justify-around gap-2">
+            <p className="">1</p>
+            <p>첼로</p>
+            <p>Fill</p>
+          </li>
+          <li className="flex items-center justify-around gap-2">
+            <p>첼로</p>
+            <p>Fill</p>
+          </li>
+          <li className="flex items-center justify-around gap-2">
+            <p className="">1</p>
+            <p>첼로</p>
+            <p>Fill</p>
+          </li>
+          <li className="flex items-center justify-around gap-2">
+            <p className="">1</p>
+            <p>첼로</p>
+            <p>Fill</p>
+          </li>
+        </ul>
+      </div>
+      <div className={div}>
+        <h1 className="text-center text-lg font-bold uppercase text-amber-500">
+          Kakao
+        </h1>
+        <ul className="border-4 p-5"></ul>
+      </div>
+      <div className={div}>
+        <h1 className="text-center text-lg font-bold uppercase text-rose-500">
+          Cupang
+        </h1>
+        <ul className="border-4 p-5"></ul>
+      </div>
+    </section>
+  );
+};
--- src/pages/shopping/Shopping.tsx
+++ src/pages/shopping/Shopping.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { ShoppingGrid } from "../../components/shopping/ShoppingGrid";
 import { ShoppingSearch } from "../../components/shopping/ShoppingSearch";
 
 export default function Shopping() {
   return (
     <main className='className="mt-0 xl:flex-row" ml-0 flex h-full w-full flex-col overflow-auto py-2 sm:ml-[250px] sm:pt-8 lg:ml-[344px] lg:h-screen'>
-      <h1 className="text-center text-4xl font-bold">Shopping Trends</h1>
+      <h1 className="text-center text-3xl font-bold">Shopping Trends</h1>
       {/* search bar */}
       <ShoppingSearch />
+      <ShoppingGrid />
     </main>
   );
 }
